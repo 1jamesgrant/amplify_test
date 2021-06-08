78350eed51de37fe04a9b743bbfb9536da7719ab
jamesgrant <james.grant@enabl-ai.com>
2021-06-08 22:48:05 +0100

    creates base amplify expo project using default authenticator

--- App.js
+++ App.js
@@ -1,15 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
-export default function App() {
+import Amplify from '@aws-amplify/core';
+import Auth from '@aws-amplify/auth';
+import config from './src/aws-exports';
+
+Amplify.configure({
+  ...config,
+  Analytics: {
+    disabled: true,
+  },
+});
+
+import { withAuthenticator } from 'aws-amplify-react-native';
+
+function App() {
+  const SignOut = async () => {
+    try {
+      await Auth.signOut();
+    } catch {
+      console.log('Error signing out: ', error);
+    }
+  };
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Welcome to the app 👋.</Text>
+      <Button title="Sign Out" color="red" onPress={SignOut} />
       <StatusBar style="auto" />
     </View>
   );
 }
+
+export default withAuthenticator(App);
 
 const styles = StyleSheet.create({
   container: {
